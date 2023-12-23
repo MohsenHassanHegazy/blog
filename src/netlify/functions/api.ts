bcf0a4243e992afbad3c3d8b86c4786a2e93725d
MohsenHassanHegazy <146730300+MohsenHassanHegazy@users.noreply.github.com>
2023-12-23 11:01:59 +0200

    git link fix

---
+++ src/netlify/functions/api.ts
@@ -0,0 +1,16 @@
+// // YOUR_BASE_DIRECTORY/netlify/functions/api.ts
+
+// import express, { Router } from "express";
+// import serverless from "ser";
+
+// import adminRoutes from "../../routes/post"
+// import authRoutes from "../../routes/auth"
+
+// const app = express();
+
+// const router = Router();
+
+// app.use(authRoutes);
+// app.use(adminRoutes);
+
+// export const handler = serverless(app);
